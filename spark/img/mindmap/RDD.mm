695d0672ec10051df67ccc8a0997e00c67d247c3
Huajian Mao <huajianmao@gmail.com>
2014-04-23 08:45:54 +0800

    Code reading about rdd

---
+++ spark/img/mindmap/RDD.mm
@@ -0,0 +1,25 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1398041483914" ID="ID_983480073" MODIFIED="1398042171715" STYLE="fork" TEXT="RDD">
+<node CREATED="1398041642788" ID="ID_1594748044" MODIFIED="1398041980499" POSITION="right" TEXT="final def partitions: Array[Partition]">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1398041710623" ID="ID_1797685748" MODIFIED="1398042177676" POSITION="left" TEXT="@transient val partitioner: Option[Partitioner]">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1398041737595" ID="ID_1352524916" MODIFIED="1398041984158" POSITION="right" TEXT="final def dependencies: Seq[Dependency[_]]">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1398041746630" ID="ID_1815370239" MODIFIED="1398042179523" POSITION="left" TEXT="final def preferredLocations(split: Partition)">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1398041810735" ID="ID_328272902" MODIFIED="1398042021211" POSITION="right" TEXT="def compute(split: Partition, context: TaskContext): Iterator[T]">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="full-3"/>
+</node>
+</node>
+</map>
