848c82fa12334c7ead667b90e825a7abd001d7a8
Huajian Mao <huajianmao@gmail.com>
2017-03-26 14:19:06 +0800

    Finish ParallelParenthesesBalancing

--- coursera/parprog1/week2/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
+++ coursera/parprog1/week2/reductions/src/main/scala/reductions/ParallelParenthesesBalancing.scala
@@ -41,22 +41,40 @@ object ParallelParenthesesBalancing {
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def balance(chars: Array[Char]): Boolean = {
-    ???
+    def loop(delta: Int, idx: Int): Int = {
+      if (delta < 0) Int.MinValue
+      else if (idx >= chars.length) delta
+      else if (chars(idx) == '(') loop(delta + 1, idx + 1)
+      else if (chars(idx) == ')') loop(delta - 1, idx + 1)
+      else loop(delta, idx + 1)
+    }
+
+    loop(0, 0) == 0
   }
 
   /** Returns `true` iff the parentheses in the input `chars` are balanced.
    */
   def parBalance(chars: Array[Char], threshold: Int): Boolean = {
 
-    def traverse(idx: Int, until: Int, arg1: Int, arg2: Int) /*: ???*/ = {
-      ???
+    def traverse(idx: Int, until: Int, leftCount: Int, rightCount: Int): (Int, Int) = {
+      if (until <= idx) (leftCount, rightCount)
+      else if (chars(idx) == '(') traverse(idx + 1, until, leftCount + 1, rightCount)
+      else if (chars(idx) == ')' && leftCount > 0) traverse(idx + 1, until, leftCount - 1, rightCount)
+      else if (chars(idx) == ')' && leftCount <= 0) traverse(idx + 1, until, leftCount, rightCount + 1)
+      else traverse(idx + 1, until, leftCount, rightCount)
     }
 
-    def reduce(from: Int, until: Int) /*: ???*/ = {
-      ???
+    def reduce(from: Int, until: Int): (Int, Int) = {
+      if (until - from <= threshold) traverse(from, until, 0, 0)
+      else {
+        val middle = (until + from) / 2
+        val (left, right) = parallel(reduce(from, middle), reduce(middle, until))
+        val matched = if (left._1 > right._2) right._2 else left._1
+        (left._1 + right._1 - matched, left._2 + right._2 - matched)
+      }
     }
 
-    reduce(0, chars.length) == ???
+    reduce(0, chars.length) == (0, 0)
   }
 
   // For those who want more:
